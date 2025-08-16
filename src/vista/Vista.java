f6d488c9f9fe0c097bf62c2814ba83d17cc70ee7
CarlosQO <carlosjqo2005@gmail.com>
2025-08-16 15:34:10 -0500

    vistaComienzo

---
+++ src/vista/Vista.java
@@ -0,0 +1,28 @@
+package vista;
+
+
+import java.awt.Container;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+public class Vista extends JFrame {
+    private JPanel datosProductos, productos,MedioPago,EmergentePago;
+    private Container contenedor;
+    public JComboBox modoDePago;
+    public JButton pagar;
+    public String ListaPago[]={};
+    private JScrollPane miScroll;
+    private GridLayout grid;
+    private FlowLayout miflow;
+    
+    public Vista(){
+        super("****** Tienda Mini ******");
+        contenedor=getContentPane();
+    }
+    
+}
