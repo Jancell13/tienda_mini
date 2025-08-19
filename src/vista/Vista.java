de50001aecf1479d9b1c7f6cf569ce050a68c306
CarlosQO <carlosjqo2005@gmail.com>
2025-08-19 17:03:37 -0500

    creacion de la vista para agregar productos

--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -1,6 +1,8 @@
 package vista;
 
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -8,66 +10,72 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
 public class Vista extends JFrame {
-    private JPanel datosProductos,productos,MedioPago,EmergentePago;
-    private Container contenedor;
+    private JFrame frame;
+    private JPanel datosProductos,productos,MedioPago;
+    private Container contenedor, contenedor2;
     public JComboBox modoDePago;
-    public JButton pagar;
+    public JButton pagar, registrar;
     public String ListaPago[]={"Hola","Mundo"};
     private JScrollPane miScroll, miScrollproductos;
-    private GridLayout grid,grid2;
     private FlowLayout miflow;
     public JTable tabla,tablaProducto;
     private DefaultTableModel campos,camposProducto;
     
     public Vista(){
         super("****** Tienda Mini ******");
-        contenedor=getContentPane();
-        miflow=new FlowLayout();
-        
-        contenedor.setLayout(miflow);
-        
+        frame = new JFrame();
+        miflow = new FlowLayout();
+        frame.setLayout(miflow);
+        contenedor= new JPanel(new GridLayout(1,2,1,1));
+        contenedor2 = new JPanel(new BorderLayout());
         
         datosProductos = new JPanel();
-        grid2=new GridLayout(2,1,2,3);
         campos=new DefaultTableModel();
         campos.addColumn("NombreProducto");
         campos.addColumn("CantidadProducto");
         campos.addColumn("PrecioTotal");
         tabla = new JTable(campos);
         miScroll=new JScrollPane(tabla);
+        miScroll.setPreferredSize(new java.awt.Dimension(320, 200));
         miScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         datosProductos.add(miScroll);
-        datosProductos.setLayout(grid2);
         
         MedioPago = new JPanel();
         modoDePago = new JComboBox(ListaPago);
         pagar = new JButton("Pagar");
-        MedioPago.add(modoDePago);
-        MedioPago.setLayout(grid2);
-        MedioPago.add(pagar);
+        registrar = new JButton("Registrar");
+        MedioPago.add(modoDePago,BorderLayout.NORTH);
+        MedioPago.add(pagar,BorderLayout.SOUTH);
+        contenedor2.add(MedioPago);
         
         productos = new JPanel();
-        grid = new GridLayout(1,1,2,3);
         camposProducto = new DefaultTableModel();
         camposProducto.addColumn("ID");
         camposProducto.addColumn("NombreProducto");
         camposProducto.addColumn("PrecioUND");
         camposProducto.addColumn("CantidadStock");
         tablaProducto = new JTable(camposProducto);
+        miScrollproductos=new JScrollPane(tablaProducto);
+        miScrollproductos.setPreferredSize(new java.awt.Dimension(450, 420));
+        miScrollproductos.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         productos.add(miScrollproductos);
-        productos.setLayout(grid);
         
+        
+
         contenedor.add(datosProductos);
-        contenedor.add(MedioPago);
-        contenedor.add(productos);
-        
-        contenedor.setVisible(true);
-        contenedor.setSize(526,720);
-        contenedor.setLocation(300, 500);
+        contenedor.add(contenedor2);
+        contenedor.setLayout(new FlowLayout());
+        frame.add(contenedor);
+        frame.add(productos);
+        frame.setLocationRelativeTo(null);
+        frame.setSize(650,720);
+        frame.setVisible(true);
+        frame.setResizable(false);
     }  
 }
--- src/vista/useVista.java
+++ src/vista/useVista.java
@@ -4,7 +4,9 @@ import javax.swing.JFrame;
 
 public class useVista {
     public static void main(String[] args) {
-        Vista v = new Vista();
-        v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //Vista v = new Vista();
+        //VistaRegistrar v = new VistaRegistrar();
+        
+        //v.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
