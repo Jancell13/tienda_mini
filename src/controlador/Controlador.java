5776558da62ce00941124058d9b85ccc262a9c99
Jancell13 <71906690+Jancell13@users.noreply.github.com>
2025-08-26 17:00:10 -0600

    cargar datos en las tablas

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -2,179 +2,267 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import java.util.List;
+
 import javax.swing.table.DefaultTableModel;
-import javax.swing.JOptionPane;
+
+import modelo.MetodoPago;
+import modelo.MetodoPagoDao;
 import modelo.Producto;
 import modelo.ProductoDao;
 import vista.Vista;
+import vista.VistaRegistrar;
 
-public class Controlador implements ActionListener {
+public class Controlador implements ActionListener, MouseListener {
     public ProductoDao pdao = new ProductoDao();
+    public MetodoPagoDao mtdao = new MetodoPagoDao();
     public Producto p = new Producto();
     public Vista view = new Vista();
     DefaultTableModel modelo = new DefaultTableModel();
     Boolean flag = false;
 
+    public Controlador(Vista v) {
+        this.view = v;
+        this.view.pagar.addActionListener(this);
+        this.view.registrar.addActionListener(this);
+        getListarProductos(view.tablaProducto);
+        getMetodosPago();
+        view.tablaProducto.addMouseListener(this);
+        view.tablaProducto.setDefaultEditor(Object.class, null);
+
+    }
+
+    @Override
+public void mouseClicked(MouseEvent me) {
+    if (me.getClickCount() == 2) {
+        int fila = view.tablaProducto.getSelectedRow();
+
+        if (fila == -1) {
+            JOptionPane.showMessageDialog(view, "error en algo");
+        } else {
+            JOptionPane.showMessageDialog(view, "Funciona");
+            String nombre = view.tablaProducto.getValueAt(fila, 0).toString();
+            String precio = view.tablaProducto.getValueAt(fila, 1).toString();
+            String cantidad = view.tablaProducto.getValueAt(fila, 2).toString();
+            
+            view.tabla.setValueAt(nombre,1,0);
+            view.tabla.setValueAt(precio,1,1);
+            view.tabla.setValueAt(cantidad,1,2);
+        }
+    }
+}
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == view.buscar) {
-            limpiarTabla();
-            getListar(view.tabla);
-            JOptionPane.showMessageDialog(view, "esta funcionando el boton");
+
+
+
+        /*
+         * if (e.getSource() == view.limpiar) {
+         * limpiarCampos();
+         * }
+         */
+        /*
+         * if (e.getSource() == view.enviar) {
+         * view.txtId.setEditable(true);
+         * flag = false;
+         * view.enviar.setEnabled(flag);
+         * updateUser(Integer.parseInt(view.txtId.getText()));
+         * limpiarCampos();
+         * limpiarTabla();
+         * getListar(view.tabla);
+         * }
+         */
+        /*
+         * if (e.getSource() == view.actualizar) {
+         * flag = true;
+         * view.enviar.setEnabled(flag);
+         * int fila = view.tabla.getSelectedRow();
+         * 
+         * if (fila == -1 && view.txtId.getText().isEmpty()) {
+         * JOptionPane.showMessageDialog(view,
+         * "debe seleccionar una fila o diligenciar todos los campos");
+         * } else {
+         * String id = view.tabla.getValueAt(fila, 0).toString();
+         * String nombre = view.tabla.getValueAt(fila, 1).toString();
+         * String apellido = view.tabla.getValueAt(fila, 2).toString();
+         * String salario = view.tabla.getValueAt(fila, 3).toString();
+         * String profesion = view.tabla.getValueAt(fila, 4).toString();
+         * String entidad = view.tabla.getValueAt(fila, 5).toString();
+         * view.txtId.setText(id);
+         * view.txtId.setEditable(false);
+         * view.txtNombre.setText(nombre);
+         * view.txtApellido.setText(apellido);
+         * view.txtSalario.setText(salario);
+         * view.txtProfesion.setText(profesion);
+         * view.listaE.setSelectedItem(entidad);
+         * }
+         * 
+         * }
+         */
+        /*
+         * if (e.getSource() == view.eliminar) {
+         * int fila = view.tabla.getSelectedRow();
+         * if (!view.txtId.getText().isEmpty()) {
+         * int id = Integer.parseInt(view.txtId.getText());
+         * deleteUser(id);
+         * } else if (fila != -1) {
+         * int id = Integer.parseInt(view.tabla.getValueAt(fila, 0).toString());
+         * deleteUser(id);
+         * } else {
+         * JOptionPane.showMessageDialog(view,
+         * "falta el id o debe seleccionar la fila que quiere eliminar");
+         * }
+         * limpiarCampos();
+         * limpiarTabla();
+         * getListar(view.tabla);
+         * }
+         */
+        /*
+         * if (e.getSource() == view.registrar) {
+         * if (!view.txtId.getText().isEmpty() && !view.txtNombre.getText().isEmpty()
+         * && !view.txtPrecio.getText().isEmpty() &&
+         * !view.txtCantidad.getText().isEmpty()
+         * && !view.txtId.getText().isEmpty() &&
+         * !view.listaE.getSelectedItem().equals("Seleccionar")) {
+         * 
+         * setAdd();
+         * } else {
+         * JOptionPane.showMessageDialog(view, "Faltan campos por ingresar");
+         * }
+         * limpiarCampos();
+         * limpiarTabla();
+         * getListar(view.tabla);
+         * }
+         */
+
+        if (e.getSource() == view.registrar) {
+
+            VistaRegistrar viewR = new VistaRegistrar();
+            viewR.setSize(520, 720);
+            viewR.setVisible(true);
+            viewR.setLocationRelativeTo(null);
+
         }
-        /* if (e.getSource() == view.limpiar) {
-            limpiarCampos();
-        } */
-       /*  if (e.getSource() == view.enviar) {
-            view.txtId.setEditable(true);
-            flag = false;
-            view.enviar.setEnabled(flag);
-            updateUser(Integer.parseInt(view.txtId.getText()));
-            limpiarCampos();
-            limpiarTabla();
-            getListar(view.tabla);
-        } */
-        /* if (e.getSource() == view.actualizar) {
-            flag = true;
-            view.enviar.setEnabled(flag);
-            int fila = view.tabla.getSelectedRow();
-
-            if (fila == -1 && view.txtId.getText().isEmpty()) {
-                JOptionPane.showMessageDialog(view, "debe seleccionar una fila o diligenciar todos los campos");
-            } else {
-                String id = view.tabla.getValueAt(fila, 0).toString();
-                String nombre = view.tabla.getValueAt(fila, 1).toString();
-                String apellido = view.tabla.getValueAt(fila, 2).toString();
-                String salario = view.tabla.getValueAt(fila, 3).toString();
-                String profesion = view.tabla.getValueAt(fila, 4).toString();
-                String entidad = view.tabla.getValueAt(fila, 5).toString();
-                view.txtId.setText(id);
-                view.txtId.setEditable(false);
-                view.txtNombre.setText(nombre);
-                view.txtApellido.setText(apellido);
-                view.txtSalario.setText(salario);
-                view.txtProfesion.setText(profesion);
-                view.listaE.setSelectedItem(entidad);
-            }
-
-        } */
-        /* if (e.getSource() == view.eliminar) {
-            int fila = view.tabla.getSelectedRow();
-            if (!view.txtId.getText().isEmpty()) {
-                int id = Integer.parseInt(view.txtId.getText());
-                deleteUser(id);
-            } else if (fila != -1) {
-                int id = Integer.parseInt(view.tabla.getValueAt(fila, 0).toString());
-                deleteUser(id);
-            } else {
-                JOptionPane.showMessageDialog(view, "falta el id o debe seleccionar la fila que quiere eliminar");
-            }
-            limpiarCampos();
-            limpiarTabla();
-            getListar(view.tabla);
-        } */
-        /* if (e.getSource() == view.registrar) {
-            if (!view.txtId.getText().isEmpty() && !view.txtNombre.getText().isEmpty()
-                    && !view.txtPrecio.getText().isEmpty() && !view.txtCantidad.getText().isEmpty()
-                    && !view.txtId.getText().isEmpty() && !view.listaE.getSelectedItem().equals("Seleccionar")) {
-
-                setAdd();
-            } else {
-                JOptionPane.showMessageDialog(view, "Faltan campos por ingresar");
-            }
-            limpiarCampos();
-            limpiarTabla();
-            getListar(view.tabla);
-        } */
 
     }
 
-    public void getListar(JTable tabla) {
+    public void getListarProductos(JTable tabla) {
         modelo = (DefaultTableModel) tabla.getModel();
         List<Producto> lista = pdao.listar();
-        Object[] object = new Object[6];
-
-        for (int i = 0; i < lista.size(); i++) {
-            object[0] = lista.get(i).getId();
-            object[1] = lista.get(i).getNombre();
-            object[2] = lista.get(i).getPrecio();
-            object[3] = lista.get(i).getCantidad();
+        Object[] object = new Object[4];
+        for (int indice = 0; indice < lista.size(); indice++) {
+            object[0] = lista.get(indice).getId();
+            object[1] = lista.get(indice).getNombre();
+            object[2] = lista.get(indice).getPrecio();
+            object[3] = lista.get(indice).getCantidad();
             modelo.addRow(object);
         }
-        view.tabla.setModel(modelo);
+        view.tablaProducto.setModel(modelo);
     }
 
-   /*  public void deleteUser(int id) {
-        int resultado = pdao.setEliminar(id);
-        if (resultado == 1) {
-            JOptionPane.showMessageDialog(view, "Se elimino correctamente");
-        } else {
-            JOptionPane.showMessageDialog(null, "Error de Eliminacion" + JOptionPane.ERROR_MESSAGE);
-        }
-    } */
-
-    /* public void setAdd() {
-        int resultado;
-        int id = Integer.parseInt(view.txtId.getText());
-        String nombre = view.txtNombre.getText();
-        String apellido = view.txtApellido.getText();
-        Double salario = Double.parseDouble(view.txtSalario.getText());
-        String profesion = view.txtProfesion.getText();
-        String entidad = view.listaE.getSelectedItem().toString();
-
-        t.setId(id);
-        t.setNombre(nombre);
-        t.setApellido(apellido);
-        t.setSalario(salario);
-        t.setProfesion(profesion);
-        t.setEntidad(entidad);
-        resultado = dao.setAgregar(t);
-        System.out.println(entidad);
-        if (resultado == 1) {
-            JOptionPane.showMessageDialog(view, "se inserto la informacion de forma correcta");
-        } else {
-            JOptionPane.showMessageDialog(null, "Error de insercion" + JOptionPane.ERROR_MESSAGE);
+    public void getMetodosPago() {
+        List<MetodoPago> mp = mtdao.listar();
+        for (int i = 0; i < mp.size(); i++) {
+            view.modoDePago.addItem(mp.get(i).getNombre());
         }
     }
 
-    public void updateUser(int id) {
-        int resultado;
-        String nombre = view.txtNombre.getText();
-        String apellido = view.txtApellido.getText();
-        Double salario = Double.parseDouble(view.txtSalario.getText());
-        String profesion = view.txtProfesion.getText();
-        String entidad = view.listaE.getSelectedItem().toString();
+    /*
+     * public void deleteUser(int id) {
+     * int resultado = pdao.setEliminar(id);
+     * if (resultado == 1) {
+     * JOptionPane.showMessageDialog(view, "Se elimino correctamente");
+     * } else {
+     * JOptionPane.showMessageDialog(null, "Error de Eliminacion" +
+     * JOptionPane.ERROR_MESSAGE);
+     * }
+     * }
+     */
 
-        t.setId(id);
-        t.setNombre(nombre);
-        t.setApellido(apellido);
-        t.setSalario(salario);
-        t.setProfesion(profesion);
-        t.setEntidad(entidad);
-
-        resultado = dao.setActualizar(t);
-
-        if (resultado == 1) {
-            JOptionPane.showMessageDialog(view, "se actualizo la informacion de forma correcta");
-        } else {
-            JOptionPane.showMessageDialog(null, "Error de actualizacion" + JOptionPane.ERROR_MESSAGE);
-        }
-    }
- */
-    public void limpiarCampos() {
-        view.txtId.setText("");
-        view.txtNombre.setText("");
-        view.txtPrecio.setText("");
-        view.txtCantidad.setText("");
-        view.listaE.setSelectedIndex(0);
-    }
-
+    /*
+     * public void setAdd() {
+     * int resultado;
+     * int id = Integer.parseInt(view.txtId.getText());
+     * String nombre = view.txtNombre.getText();
+     * String apellido = view.txtApellido.getText();
+     * Double salario = Double.parseDouble(view.txtSalario.getText());
+     * String profesion = view.txtProfesion.getText();
+     * String entidad = view.listaE.getSelectedItem().toString();
+     * 
+     * t.setId(id);
+     * t.setNombre(nombre);
+     * t.setApellido(apellido);
+     * t.setSalario(salario);
+     * t.setProfesion(profesion);
+     * t.setEntidad(entidad);
+     * resultado = dao.setAgregar(t);
+     * System.out.println(entidad);
+     * if (resultado == 1) {
+     * JOptionPane.showMessageDialog(view,
+     * "se inserto la informacion de forma correcta");
+     * } else {
+     * JOptionPane.showMessageDialog(null, "Error de insercion" +
+     * JOptionPane.ERROR_MESSAGE);
+     * }
+     * }
+     * 
+     * public void updateUser(int id) {
+     * int resultado;
+     * String nombre = view.txtNombre.getText();
+     * String apellido = view.txtApellido.getText();
+     * Double salario = Double.parseDouble(view.txtSalario.getText());
+     * String profesion = view.txtProfesion.getText();
+     * String entidad = view.listaE.getSelectedItem().toString();
+     * 
+     * t.setId(id);
+     * t.setNombre(nombre);
+     * t.setApellido(apellido);
+     * t.setSalario(salario);
+     * t.setProfesion(profesion);
+     * t.setEntidad(entidad);
+     * 
+     * resultado = dao.setActualizar(t);
+     * 
+     * if (resultado == 1) {
+     * JOptionPane.showMessageDialog(view,
+     * "se actualizo la informacion de forma correcta");
+     * } else {
+     * JOptionPane.showMessageDialog(null, "Error de actualizacion" +
+     * JOptionPane.ERROR_MESSAGE);
+     * }
+     * }
+     */
+    /*
+     * public void limpiarCampos() {
+     * view.txtId.setText("");
+     * view.txtNombre.setText("");
+     * view.txtPrecio.setText("");
+     * view.txtCantidad.setText("");
+     * view.listaE.setSelectedIndex(0);
+     * }
+     */
     public void limpiarTabla() {
         for (int i = 0; i < view.tabla.getRowCount(); i++) {
             modelo.removeRow(i);
             i = i - 1;
         }
     }
+
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
 }
---
+++ src/controlador/ControladorRegistrarProduc.java
@@ -0,0 +1,244 @@
+package controlador;
+
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import java.util.List;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JOptionPane;
+
+import modelo.Producto;
+import modelo.ProductoDao;
+import vista.VistaRegistrar;
+
+public class ControladorRegistrarProduc implements ActionListener, MouseListener {
+
+    public ProductoDao pdao = new ProductoDao();
+    public Producto p = new Producto();
+    public VistaRegistrar viewR = new VistaRegistrar();
+    DefaultTableModel modelo = new DefaultTableModel();
+    boolean flag = false;
+
+    public ControladorRegistrarProduc(VistaRegistrar v) {
+        this.viewR = v;
+        this.viewR.buscar.addActionListener(this);
+        this.viewR.eliminar.addActionListener(this);
+        this.viewR.actualizar.addActionListener(this);
+        this.viewR.registrar.addActionListener(this);
+        this.viewR.enviar.setEnabled(false);
+        this.viewR.enviar.addActionListener(this);
+        this.viewR.limpiar.addActionListener(this);
+        getListar(viewR.tabla);
+        viewR.tabla.addMouseListener(this);
+        viewR.tabla.setDefaultEditor(Object.class, null);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+        if (me.getClickCount() == 2) {
+            flag = true;
+            viewR.enviar.setEnabled(flag);
+            int fila = viewR.tabla.getSelectedRow();
+
+            if (fila == -1 && viewR.tid.getText().isEmpty()) {
+                // falta comparacion de todos los campos
+                JOptionPane.showMessageDialog(viewR, "debe seleccionar la fila o dilgenciar todos los campos");
+            } else {
+                int id = Integer.parseInt(viewR.tabla.getValueAt(fila, 0).toString());
+                String nombre = viewR.tabla.getValueAt(fila, 1).toString();
+                String precio = viewR.tabla.getValueAt(fila, 2).toString();
+                String cantidad = viewR.tabla.getValueAt(fila, 3).toString();
+                viewR.tid.setText("" + id);
+                viewR.tid.setEnabled(false);
+                viewR.tnombre.setText(nombre);
+                viewR.tprecio.setText(precio);
+                viewR.tcantidad.setText(cantidad);
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+
+        if (ae.getSource() == viewR.buscar) {
+            limpiarTabla();
+            getListar(viewR.tabla);
+            JOptionPane.showMessageDialog(viewR, "Consulta exitosa");
+        }
+
+        if (ae.getSource() == viewR.registrar) {
+            if (!viewR.tid.getText().isEmpty() && !viewR.tnombre.getText().isEmpty()
+                    && !viewR.tprecio.getText().isEmpty() && !viewR.tcantidad.getText().isEmpty()) {
+                setAdd();
+            } else {
+                JOptionPane.showMessageDialog(viewR, "faltan campos por diligenciar");
+            }
+            limpiarTabla();
+            limpiarCampos();
+            getListar(viewR.tabla);
+
+        }
+
+        if (ae.getSource() == viewR.eliminar) {
+            int fila = viewR.tabla.getSelectedRow();
+            JOptionPane.showMessageDialog(viewR, "que valor contiene la fila " + fila);
+
+            if (!viewR.tid.getText().isEmpty()) {
+                int id = Integer.parseInt(viewR.tid.getText());
+                setDelete(id);
+
+            } else if (fila != -1) {
+                int id = Integer.parseInt(viewR.tabla.getValueAt(fila, 0).toString());
+                setDelete(id);
+            } else {
+                JOptionPane.showMessageDialog(viewR, "falta el id o debe seleccionar la fila que quiere eliminar");
+            }
+
+            limpiarTabla();
+            limpiarCampos();
+            getListar(viewR.tabla);
+        }
+
+        if (ae.getSource() == viewR.limpiar) {
+            limpiarCampos();
+        }
+
+        if (ae.getSource() == viewR.actualizar) {
+            flag = true;
+            viewR.enviar.setEnabled(flag);
+            int fila = viewR.tabla.getSelectedRow();
+
+            if (fila == -1 && viewR.tid.getText().isEmpty()) {
+                // falta comparacion de todos los campos
+                JOptionPane.showMessageDialog(viewR, "debe seleccionar la fila o dilgenciar todos los campos");
+            } else {
+                int id = Integer.parseInt(viewR.tabla.getValueAt(fila, 0).toString());
+                String nombre = viewR.tabla.getValueAt(fila, 1).toString();
+                String precio = viewR.tabla.getValueAt(fila, 2).toString();
+                String cantidad = viewR.tabla.getValueAt(fila, 3).toString();
+                viewR.tid.setText("" + id);
+                viewR.tid.setEnabled(false);
+                viewR.tnombre.setText(nombre);
+                viewR.tprecio.setText(precio);
+                viewR.tcantidad.setText(cantidad);
+
+            }
+        }
+        if (ae.getSource() == viewR.enviar && flag == true) {
+
+            int id = Integer.parseInt(viewR.tid.getText());
+            setUpdate(id);
+            flag = false;
+            viewR.tid.setEditable(true);
+            limpiarTabla();
+            limpiarCampos();
+            getListar(viewR.tabla);
+        }
+    }
+
+    public void getListar(JTable tabla) {
+        modelo = (DefaultTableModel) tabla.getModel();
+        List<Producto> lista = pdao.listar();
+        Object[] object = new Object[6];
+        for (int indice = 0; indice < lista.size(); indice++) {
+            object[0] = lista.get(indice).getId();
+            object[1] = lista.get(indice).getNombre();
+            object[2] = lista.get(indice).getPrecio();
+            object[3] = lista.get(indice).getCantidad();
+            modelo.addRow(object);
+        }
+        viewR.tabla.setModel(modelo);
+    }
+
+    public void setAdd() {
+        int resultado;
+        int id = Integer.parseInt(viewR.tid.getText().toString());
+        String nombre = viewR.tnombre.getText();
+        double precio = Double.parseDouble(viewR.tprecio.getText().toString());
+        int cantidad = Integer.parseInt(viewR.tcantidad.getText().toString());
+
+        p.setId(id);
+        p.setNombre(nombre);
+        p.setPrecio(precio);
+        p.setCantidad(cantidad);
+
+        resultado = pdao.setAgregar(p);
+
+        if (resultado == 1) {
+            JOptionPane.showMessageDialog(viewR, "se inserto la informacion de forma correcta");
+        } else {
+            JOptionPane.showMessageDialog(viewR, "Error de inserccion " + JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void setDelete(int id) {
+        int resultado;
+        resultado = pdao.setEliminar(id);
+
+        if (resultado == 1) {
+            JOptionPane.showMessageDialog(viewR, "se elimino correctamente");
+        } else {
+            JOptionPane.showMessageDialog(viewR, "Error de eliminacion");
+        }
+    }
+
+    public void setUpdate(int id) {
+        int resultado;
+
+        String nombre = viewR.tnombre.getText();
+        double precio = Double.parseDouble(viewR.tprecio.getText().toString());
+        int cantidad = Integer.parseInt(viewR.tcantidad.getText().toString());
+
+        p.setId(id);
+        p.setNombre(nombre);
+        p.setPrecio(precio);
+        p.setCantidad(cantidad);
+        resultado = pdao.setActualizar(p);
+
+        if (resultado == 1) {
+            JOptionPane.showMessageDialog(viewR, "se actualizo correctamente");
+        } else {
+            JOptionPane.showMessageDialog(viewR, "Error de actualizacion " + JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void limpiarTabla() {
+        for (int i = 0; i < viewR.tabla.getRowCount(); i++) {
+            modelo.removeRow(i);
+            i = i - 1;
+        }
+    }
+
+    public void limpiarCampos() {
+        viewR.tid.setText("");
+        viewR.tnombre.setText("");
+        viewR.tprecio.setText("");
+        viewR.tcantidad.setText("");
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+}
