2321329ceadcbe80d4ee7ea3562efbba6e3ee9e1
Judava <juandavidvaldes97@gmail.com>
2025-08-30 15:19:46 -0500

    Add files via upload

--- tienda.sql
+++ tienda.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-08-2025 a las 18:34:26
+-- Tiempo de generación: 30-08-2025 a las 22:19:05
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
